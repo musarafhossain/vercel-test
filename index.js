6e8ab1af4c8a79f1895bbe1dc78b052509945908
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2024-08-25 13:50:38 +0530

    add more testing route

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('<h1>Hello, World!</h1><a href="/profile">Go Profile</a>');
+});
+
+app.get('profile/', (req, res) => {
+  res.send('<h2>This is profile page.</h2><a href="/">Go Home</a>');
 });
 
 app.listen(PORT, () => {
